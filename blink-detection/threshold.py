b75f803d8cc902f5612b29440a6d551727ffb373
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-07-09 12:02:25 -0400

    Update threshold.py

--- blink-detection/threshold.py
+++ blink-detection/threshold.py
@@ -39,4 +39,24 @@ def two_frame_gap_thresh(EARs, avg_dist, dists):
     bottom_EARs = low_EARs[int(len(low_EARs) * .9) : int(len(low_EARs) * 1.0)]
     threshold = mean([mean(top_EARs),mean(bottom_EARs))
 
-
+def compare_IOUs(EARs, gt_pairs):
+    (avg_dist, dists) = frame_to_frame_EAR_diff(EARs)
+    
+    thresh_2frame = two_frame_gap_thresh(EARs, avg_dist, dists)
+    pred_pairs_2frame = get_pred_blink_pairs(EARs, thresh_2frame)
+    IOU_vals_2frame = IOU_eval(gt_pairs, pred_pairs_2frame)
+    
+    pred_pairs_2 = get_pred_blink_pairs(EARs, .2)
+    IOU_vals_2 = IOU_eval(gt_pairs, pred_pairs_2)
+    
+    pred_pairs_25 = get_pred_blink_pairs(EARs, .25)
+    IOU_vals_25 = IOU_eval(gt_pairs, pred_pairs_25)
+    
+    pred_pairs_3 = get_pred_blink_pairs(EARs, .3)
+    IOU_vals_3 = IOU_eval(gt_pairs, pred_pairs_3)
+    
+    pred_pairs_35 = get_pred_blink_pairs(EARs, .35)
+    IOU_vals_35 = IOU_eval(gt_pairs, pred_pairs_35)
+    
+    
+    
