171d261fc7fa6165cda4afd1ce562513cc926d04
Peter <petedecker@gmail.com>
2019-07-10 14:45:10 -0400

    commiting to my stuff

--- blink-detection/main.py
+++ blink-detection/main.py
@@ -22,7 +22,7 @@ GLOBAL VARS
 
 # eye aspect ratio to indicate blink 
 EYE_AR_THRESH = 0.3
-column_name = ['video_file_avi', 'video_file_mp4', 'dat_file', 'text_file', 'path', 'png_file', 'folder']
+column_name = ['video_file_avi', 'dat_file', 'text_file', 'path', 'png_file', 'folder']
 df_videodata = pd.DataFrame(columns=column_name)
 
 
@@ -30,6 +30,7 @@ df_videodata = pd.DataFrame(columns=column_name)
 def read_data(dataset_name):
     mypath = os.path.join(os.getcwd(), 'data_sets\\', dataset_name)
     for (dirpath, dirnames, filenames) in os.walk(mypath):
+        print(filenames)
         if not filenames:
             print("empty")
         if filenames:
@@ -44,27 +45,29 @@ def read_data(dataset_name):
     return df_videodata
 
 def get_VIDEO_FILENAME(i):
-    print('video file: ' + df_videodata.at[i, 'video_file_mp4'])
-    return df_videodata.at[i, 'video_file_mp4']
-    #return '000001M_FBN.mp4'
+      return df_videodata.at[i, 'video_file_avi']
+
 
 def get_TAG_FILENAME(i):
     return os.path.join(df_videodata.iloc[i]['path'], df_videodata.iloc[i]['dat_file'])
-   
+
+
 def get_PNG_FILENAME(i):
     return df_videodata.at[i, 'png_file']
+
 
 def get_PATH(i):
     return df_videodata.at[i, 'path']
 
+
 def get_FOLDERNAME(i):
     return df_videodata.at[i, 'folder']
+
 
 def get_GT_blinks(tag_filename):
     # the first and second columns store the frame # and the blink value
     # -1 = no blink, all other numbers tell which blink you're on (e.g. 1,2,3,...)
     mypath = os.path.join(os.getcwd(), 'data_sets\\', tag_filename)
-    #mypath = tag_filename
     rows_to_skip = 0
     # find the number of headerlines to be skipped (varies file to file)
     searchfile = open(mypath, "r")
@@ -78,17 +81,14 @@ def get_GT_blinks(tag_filename):
 
 def main():
     
-    read_data('zju_test')
+    read_data('zju')
     num_rows = df_videodata.shape[0]
 
     for i in range(num_rows):
         video_filename = get_VIDEO_FILENAME(i)
-        print('video file name: ' + video_filename)
-
         tag_filename = get_TAG_FILENAME(i)
         png_filename = get_PNG_FILENAME(i)
         path = get_PATH(i)
-        print('path: ', path)
         video_filename = os.path.join(path, video_filename)
         gt_blinks = get_GT_blinks(tag_filename)
         (detector, predictor, lStart, lEnd, rStart, rEnd) = vid.init_detector_predictor()
